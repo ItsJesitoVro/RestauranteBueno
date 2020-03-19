26f2b0eb39e44eda2aa2882f06f5135cd2695a04
Ricardo Jimenez <Ricardo-Jimenez-1984@users.noreply.github.com>
2020-03-18 21:53:50 -0600

    Fondo
    Agregue el fondo a los ejecutables con un archivo .css

--- src/Project/MainPage.java
+++ src/Project/MainPage.java
@@ -14,10 +14,9 @@ public static void main(String[] args) {
 	        launch(args);
 	    }
 	
-public void start (Stage primaryStage) throws IOException{                                                                                                                                                                                                                                                                                                    
-	        
+public void start (Stage primaryStage) throws IOException{                                                                                                                                                                                                                                                                                                  
 	        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root);
+		Scene scene = new Scene(root);
 	        primaryStage.setTitle("RESTAURANTE"); 
 	        primaryStage.setScene(scene); 
 	        primaryStage.show(); 
